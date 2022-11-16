7dda3b7819508576e463e00aab180b577777eb48
Tom Forbes <tom@tomforb.es>
2022-11-16 11:07:11 +0000

    {"name":"learnx-zh","version":"2022.11.16.0","file":"learnx_zh-2022.11.16.0-py3-none-any.whl","path":"learnx-zh/learnx_zh-2022.11.16.0-py3-none-any.whl"}

---
+++ learnx-zh/learnx_zh-2022.11.16.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""learnx-zh
+https://github.com/apachecn/learnx-zh"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.16.0"
