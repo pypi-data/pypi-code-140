47ec4b6d8aa2f9675b59b23277d6fc9a7f5bdb33
Tom Forbes <tom@tomforb.es>
2022-11-16 18:03:01 +0000

    {"name":"pairs-unina","version":"0.1.2","file":"PaIRS_UniNa-0.1.2-cp38-cp38-macosx_12_0_universal2.whl","path":"pairs-unina/PaIRS_UniNa-0.1.2-cp38-cp38-macosx_12_0_universal2.whl"}

---
+++ pairs-unina/PaIRS_UniNa-0.1.2-cp38-cp38-macosx_12_0_universal2.whl/Log_Tab.py
@@ -0,0 +1,42 @@
+from .ui_Log_Tab import*
+from .PaIRS_pypacks import*
+from .addwidgets_ps import*
+
+class Log_Tab(QWidget):
+
+    def __init__(self,parent):
+        super().__init__(parent)
+
+        ui=Ui_LogTab()
+        ui.setupUi(self)
+
+        font=self.font()
+        font.setFamily(fontName)
+        self.setFont(font)
+        c=self.findChildren(QObject)
+        for w in c:
+            if hasattr(w,'setFont'):
+                font=w.font()
+                font.setFamily(fontName)
+                w.setFont(font)
+        self.ui=ui
+
+        
+        ui.log.setText('')
+        self.logWrite('Welcome to PaIRS interface!\nEnjoy it!\n\n')
+
+
+    def logWrite(self, text):
+        cursor = self.ui.log.textCursor() 
+        cursor.movePosition(QtGui.QTextCursor.End)
+        cursor.insertText(text)
+        self.ui.log.setTextCursor(cursor)
+        self.ui.log.ensureCursorVisible()
+
+if __name__ == "__main__":
+    import sys
+    app = QApplication(sys.argv)
+    app.setStyle('Fusion')
+    object = Log_Tab(None)
+    object.show()
+    sys.exit(app.exec())
