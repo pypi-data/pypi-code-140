6bbdec5457ba14d2b9da969e319c93dd1710d549
Tom Forbes <tom@tomforb.es>
2022-11-17 13:35:33 +0000

    {"name":"xtutorial-zh","version":"2022.11.17.0","file":"xtutorial_zh-2022.11.17.0-py3-none-any.whl","path":"xtutorial-zh/xtutorial_zh-2022.11.17.0-py3-none-any.whl"}

---
+++ xtutorial-zh/xtutorial_zh-2022.11.17.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""xtutorial-zh
+https://github.com/apachecn/xtutorial-zh"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.17.0"
