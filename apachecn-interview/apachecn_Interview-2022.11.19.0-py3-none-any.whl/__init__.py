448f870c42e583b4a930ac0b8ce92b7dd7acd5b2
Tom Forbes <tom@tomforb.es>
2022-11-19 03:47:49 +0000

    {"name":"apachecn-interview","version":"2022.11.19.0","file":"apachecn_Interview-2022.11.19.0-py3-none-any.whl","path":"apachecn-interview/apachecn_Interview-2022.11.19.0-py3-none-any.whl"}

---
+++ apachecn-interview/apachecn_Interview-2022.11.19.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""apachecn-Interview
+https://github.com/apachecn/apachecn-Interview"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.19.0"
