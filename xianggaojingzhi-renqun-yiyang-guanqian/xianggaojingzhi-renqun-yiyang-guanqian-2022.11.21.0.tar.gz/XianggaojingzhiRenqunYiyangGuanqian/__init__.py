40439cadbdaafb19b62dcc9af339737ee8796184
Tom Forbes <tom@tomforb.es>
2022-11-21 14:55:52 +0000

    {"name":"xianggaojingzhi-renqun-yiyang-guanqian","version":"2022.11.21.0","file":"xianggaojingzhi-renqun-yiyang-guanqian-2022.11.21.0.tar.gz","path":"xianggaojingzhi-renqun-yiyang-guanqian/xianggaojingzhi-renqun-yiyang-guanqian-2022.11.21.0.tar.gz"}

---
+++ xianggaojingzhi-renqun-yiyang-guanqian/xianggaojingzhi-renqun-yiyang-guanqian-2022.11.21.0.tar.gz/XianggaojingzhiRenqunYiyangGuanqian/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""xianggaojingzhi-renqun-yiyang-guanqian
+https://github.com/apachecn/xianggaojingzhi-renqun-yiyang-guanqian"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.21.0"
