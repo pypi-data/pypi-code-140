67da1b299485a90ad2f8fc1853d016224b587c6a
Tom Forbes <tom@tomforb.es>
2022-11-21 14:47:23 +0000

    {"name":"laopu-shizi-201712-201802","version":"2022.11.21.0","file":"laopu-shizi-201712-201802-2022.11.21.0.tar.gz","path":"laopu-shizi-201712-201802/laopu-shizi-201712-201802-2022.11.21.0.tar.gz"}

---
+++ laopu-shizi-201712-201802/laopu-shizi-201712-201802-2022.11.21.0.tar.gz/LaopuShizi201712201802/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""laopu-shizi-201712-201802
+https://github.com/apachecn/laopu-shizi-201712-201802"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.21.0"
