c6938afead18b9bf5eba9abf0490063bdad5c3b9
Tom Forbes <tom@tomforb.es>
2022-11-21 14:57:03 +0000

    {"name":"xulai-geihaizide-bowuxue-201707-09","version":"2022.11.21.0","file":"xulai_geihaizide_bowuxue_201707_09-2022.11.21.0-py3-none-any.whl","path":"xulai-geihaizide-bowuxue-201707-09/xulai_geihaizide_bowuxue_201707_09-2022.11.21.0-py3-none-any.whl"}

---
+++ xulai-geihaizide-bowuxue-201707-09/xulai_geihaizide_bowuxue_201707_09-2022.11.21.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""xulai-geihaizide-bowuxue-201707-09
+https://github.com/apachecn/xulai-geihaizide-bowuxue-201707-09"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.21.0"
