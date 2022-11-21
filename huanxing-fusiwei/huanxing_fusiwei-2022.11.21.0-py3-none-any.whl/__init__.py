ee1430d2d3fc5e89ed99d9c90fc777b33a0cfe34
Tom Forbes <tom@tomforb.es>
2022-11-21 14:42:03 +0000

    {"name":"huanxing-fusiwei","version":"2022.11.21.0","file":"huanxing_fusiwei-2022.11.21.0-py3-none-any.whl","path":"huanxing-fusiwei/huanxing_fusiwei-2022.11.21.0-py3-none-any.whl"}

---
+++ huanxing-fusiwei/huanxing_fusiwei-2022.11.21.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""huanxing-fusiwei
+https://github.com/apachecn/huanxing-fusiwei"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.21.0"
