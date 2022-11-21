b2e9d61066d15d73ef4db5270049babdc097524f
Tom Forbes <tom@tomforb.es>
2022-11-21 14:59:48 +0000

    {"name":"zenyang-chengwei-daituanduide-gaoshou","version":"2022.11.21.0","file":"zenyang-chengwei-daituanduide-gaoshou-2022.11.21.0.tar.gz","path":"zenyang-chengwei-daituanduide-gaoshou/zenyang-chengwei-daituanduide-gaoshou-2022.11.21.0.tar.gz"}

---
+++ zenyang-chengwei-daituanduide-gaoshou/zenyang-chengwei-daituanduide-gaoshou-2022.11.21.0.tar.gz/ZenyangChengweiDaituanduideGaoshou/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""zenyang-chengwei-daituanduide-gaoshou
+https://github.com/apachecn/zenyang-chengwei-daituanduide-gaoshou"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.21.0"
