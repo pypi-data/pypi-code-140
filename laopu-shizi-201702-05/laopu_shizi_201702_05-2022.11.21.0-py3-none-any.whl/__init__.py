70258ac21702fa94fe1768da085cec7cde71c0e0
Tom Forbes <tom@tomforb.es>
2022-11-21 14:46:29 +0000

    {"name":"laopu-shizi-201702-05","version":"2022.11.21.0","file":"laopu_shizi_201702_05-2022.11.21.0-py3-none-any.whl","path":"laopu-shizi-201702-05/laopu_shizi_201702_05-2022.11.21.0-py3-none-any.whl"}

---
+++ laopu-shizi-201702-05/laopu_shizi_201702_05-2022.11.21.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""laopu-shizi-201702-05
+https://github.com/apachecn/laopu-shizi-201702-05"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.21.0"
