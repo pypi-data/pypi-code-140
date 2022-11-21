e7bfae26bc65174c31817c3cd01fa7fa59bc0492
Tom Forbes <tom@tomforb.es>
2022-11-21 14:50:54 +0000

    {"name":"ruhe-guanhao-haizide-shili","version":"2022.11.21.0","file":"ruhe_guanhao_haizide_shili-2022.11.21.0-py3-none-any.whl","path":"ruhe-guanhao-haizide-shili/ruhe_guanhao_haizide_shili-2022.11.21.0-py3-none-any.whl"}

---
+++ ruhe-guanhao-haizide-shili/ruhe_guanhao_haizide_shili-2022.11.21.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""ruhe-guanhao-haizide-shili
+https://github.com/apachecn/ruhe-guanhao-haizide-shili"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.21.0"
